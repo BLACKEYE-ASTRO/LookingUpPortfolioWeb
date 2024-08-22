ab0a4f953d719a9dbcbac9d3649d9bc27ba49c22
BlackEye <akashshingare2003@gmail.com>
2024-08-22 22:37:33 +0530

    Updated

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -12,28 +12,30 @@ const ImageSlider = ({ images }) => {
     };
 
     return (
-        <div className="flex justify-center p-10">
+        <div className="relative flex justify-center items-center w-full max-w-screen-xl mx-auto p-4">
             <button
-                className="absolute top-[500px] md:top-[600px] left-1 md:left-[200px] transform -translate-y-1/2 text-white bg-white  py-2 px-4 rounded-md"
+                className="absolute top-1/2 left-4 transform -translate-y-1/2 text-white bg-white bg-opacity-10 backdrop-blur-md border border-white border-opacity-20 hover:bg-opacity-40 py-2 px-4 rounded-full transition duration-300"
                 onClick={prevSlide}
             >
-                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
+                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd">
                     <path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm3 5.753l-6.44 5.247 6.44 5.263-.678.737-7.322-6 7.335-6 .665.753z" />
                 </svg>
             </button>
+            <div className="w-full overflow-hidden">
+                <img
+                    className="w-full h-[500px] md:h-[600px] object-contain"
+                    src={images[currentImage]}
+                    alt={`Slide ${currentImage}`}
+                />
+            </div>
             <button
-                className="absolute top-[500px] md:top-[600px] right-1 md:right-[200px] transform -translate-y-1/2 text-white bg-white py-2 px-4 rounded-md"
+                className="absolute top-1/2 right-4 transform -translate-y-1/2 text-white bg-white bg-opacity-10 backdrop-blur-md border border-white border-opacity-20 hover:bg-opacity-40 py-2 px-4 rounded-full transition duration-300"
                 onClick={nextSlide}
             >
-                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
+                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd">
                     <path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm-3 5.753l6.44 5.247-6.44 5.263.678.737 7.322-6-7.335-6-.665.753z" />
                 </svg>
             </button>
-            <img
-                className=" w-full md:h-[800px] object-cover aspect-auto"
-                src={images[currentImage]}
-                alt={`Slide ${currentImage}`} />
-
         </div>
     );
 };
--- src/components/UpcomingEvents.js
+++ src/components/UpcomingEvents.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import ImageSlider from './ImageSlider';
+import upevent1 from "../assests/img/nepal-pharma-dec23_page-0001.jpg";
+import upevent2 from "../assests/img/Looking up Media partner.jpg";
 import upevent3 from "../assests/img/pharma_live_expo-min.jpg";
 import upevent4 from "../assests/img/amp_tech_haridwar-min.jpg";
-import upevent2 from "../assests/img/Looking up Media partner.jpg";
-import upevent1 from "../assests/img/nepal-pharma-dec23_page-0001.jpg";
 import upevent5 from "../assests/img/ugandapharma2024-VB_page-0001.jpg";
-import upevent6 from "../assests/img/12-14_April_Event.jpg";
+import upevent6 from "../assests/img/26_April_Event.jpeg";
 import upevent7 from "../assests/img/25-26_April_Event.jpeg";
-import upevent8 from "../assests/img/26_April_Event.jpeg";
+import upevent8 from "../assests/img/12-14_April_Event.jpg";
+import upevent9 from "../assests/img/April2025-17-18-19.jpeg";
+import upevent10 from "../assests/img/Feb2025-20-21-22.jpeg";
+import upevent11 from "../assests/img/August-30-31.jpeg";
+import upevent12 from "../assests/img/August-29-30-31.jpeg";
+import upevent13 from "../assests/img/August-8-9-10.jpeg";
 
 export default function UpcomingEvents() {
-  const images = [upevent6,upevent7,upevent8,upevent5, upevent4, upevent3, upevent2,upevent1];
+  const images = [upevent13,upevent12,upevent11,upevent10,upevent9,upevent8,upevent7,upevent6,upevent5, upevent4, upevent3, upevent2,upevent1];
 
   return (
     <>
