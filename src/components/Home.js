13c530b7565ec50e4a082dcb5386daa6c0119bf4
BLACKEYE-ASTRO <akashshingare2003@gmail.com>
2025-03-22 18:02:51 +0530

    All updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,66 @@
 import React, { useState, useEffect } from 'react';
 import { Typewriter } from 'react-simple-typewriter';
-import newEvent from '../assets/img/A4 Bengaluru 2025 1.png';
+// import newEvent from '../assets/img/A4 Bengaluru 2025 1.png';
 import m1 from "../assets/img/LookingUp_Brochure Edition 1-min.jpg";
 import m2 from "../assets/img/LookingUp_Brochure Edition 2-min.jpg";
 import m3 from "../assets/img/LookingUp_Brochure Edition 3-min.jpg";
 import m4 from "../assets/img/LookingUp_Brochure Edition 4-min.jpg";
 import m5 from "../assets/img/LookingUp_Brochure Edition 5-min.jpg";
 import { Banner } from './Banner';
-import comp1 from "../assets/companies/Looking Up_Co. Logo_page-0001.jpg";
-// ... other imports ...
+import comp1 from "../assets/companies/Looking Up_Co. Logo_page-0031.png";
+import comp2 from "../assets/companies/Looking Up_Co. Logo_page-0032.png";
+import comp3 from "../assets/companies/Looking Up_Co. Logo_page-0033.png";
+import comp4 from "../assets/companies/Looking Up_Co. Logo_page-0034.png";
+import comp5 from "../assets/companies/Looking Up_Co. Logo_page-0005.png";
+import comp6 from "../assets/companies/Looking Up_Co. Logo_page-0035.png";
+import comp7 from "../assets/companies/Looking Up_Co. Logo_page-0030.png";
+import comp8 from "../assets/companies/Looking Up_Co. Logo_page-0001.jpg";
+import comp9 from "../assets/companies/Looking Up_Co. Logo_page-0002.jpg";
+import comp10 from "../assets/companies/Looking Up_Co. Logo_page-0003.jpg";
+import comp11 from "../assets/companies/Looking Up_Co. Logo_page-0004.jpg";
+import comp12 from "../assets/companies/Looking Up_Co. Logo_page-0005.jpg";
+import comp13 from "../assets/companies/Looking Up_Co. Logo_page-0006.jpg";
+import comp14 from "../assets/companies/Looking Up_Co. Logo_page-0007.jpg";
+import comp15 from "../assets/companies/Looking Up_Co. Logo_page-0008.jpg";
+import comp16 from "../assets/companies/Looking Up_Co. Logo_page-0009.jpg";
+import comp17 from "../assets/companies/Looking Up_Co. Logo_page-0029.jpg";
+import comp18 from "../assets/companies/Looking Up_Co. Logo_page-0012.jpg";
+import comp19 from "../assets/companies/Looking Up_Co. Logo_page-0013.jpg";
+import comp20 from "../assets/companies/Looking Up_Co. Logo_page-0014.jpg";
+import comp21 from "../assets/companies/Looking Up_Co. Logo_page-0015.jpg";
+import comp22 from "../assets/companies/Looking Up_Co. Logo_page-0016.jpg";
+import comp23 from "../assets/companies/Looking Up_Co. Logo_page-0017.jpg";
+import comp24 from "../assets/companies/Looking Up_Co. Logo_page-0018.jpg";
+import comp25 from "../assets/companies/Looking Up_Co. Logo_page-0019.jpg";
+import comp26 from "../assets/companies/Looking Up_Co. Logo_page-0020.jpg";
+import comp27 from "../assets/companies/Looking Up_Co. Logo_page-0021.jpg";
+import comp28 from "../assets/companies/Looking Up_Co. Logo_page-0022.jpg";
+import comp29 from "../assets/companies/Looking Up_Co. Logo_page-0023.jpg";
+import comp30 from "../assets/companies/Looking Up_Co. Logo_page-0024.jpg";
+import comp31 from "../assets/companies/Looking Up_Co. Logo_page-0025.jpg";
+import comp32 from "../assets/companies/Looking Up_Co. Logo_page-0026.jpg";
+import comp33 from "../assets/companies/Looking Up_Co. Logo_page-0027.jpg";
+import comp34 from "../assets/companies/Looking Up_Co. Logo_page-0028.jpg";
+import comp35 from "../assets/companies/Looking Up_Co. Logo_page-0011.jpg";
 
-const images = [comp1, /* other images */].map((image) => ({
+const images = [comp1,comp2,comp3,comp4,comp5,comp6,comp7,comp8,comp9,comp10,comp11,comp12,comp13,comp14,comp15,comp16,comp17,comp18,comp19,comp20,comp21,comp22,comp23,comp24,comp25,comp26,comp27,comp28,comp29,comp30,comp31,comp32,comp33,comp34,comp35].map((image) => ({
     image
 }));
 
 export default function Home() {
-  const [isPopupVisible, setIsPopupVisible] = useState(false);
+  // const [isPopupVisible, setIsPopupVisible] = useState(false);
 
-  useEffect(() => {
-    setIsPopupVisible(true); // Show popup when component mounts
-  }, []);
+  // useEffect(() => {
+  //   setIsPopupVisible(true); // Show popup when component mounts
+  // }, []);
 
-  const closePopup = () => {
-    setIsPopupVisible(false); // Hide popup on close
-  };
+  // const closePopup = () => {
+  //   setIsPopupVisible(false); // Hide popup on close
+  // };
 
   return (
     <>
-      {isPopupVisible && (
+      {/* {isPopupVisible && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
           <div className="relative bg-white p-4 rounded-lg shadow-lg w-11/12 max-w-md mx-auto">
             <button
@@ -43,7 +76,7 @@ export default function Home() {
             />
           </div>
         </div>
-      )}
+      )} */}
 
       <div className="bg-[url('/src/assets/img/background.jpg')] bg-cover bg-center w-full py-32 md:py-60">
         <div className="max-w-screen-lg mx-auto text-center font-bold px-4">
--- src/components/UpcomingEvents.js
+++ src/components/UpcomingEvents.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import ImageSlider from './ImageSlider';
-import upevent1 from "../assets/img/nepal-pharma-dec23_page-0001.jpg";
-import upevent2 from "../assets/img/Looking up Media partner.jpg";
-import upevent3 from "../assets/img/pharma_live_expo-min.jpg";
-import upevent4 from "../assets/img/amp_tech_haridwar-min.jpg";
-import upevent5 from "../assets/img/ugandapharma2024-VB_page-0001.jpg";
-import upevent6 from "../assets/img/26_April_Event.jpeg";
-import upevent7 from "../assets/img/25-26_April_Event.jpeg";
-import upevent8 from "../assets/img/12-14_April_Event.jpg";
-import upevent9 from "../assets/img/April2025-17-18-19.jpeg";
-import upevent10 from "../assets/img/Feb2025-20-21-22.jpeg";
-import upevent11 from "../assets/img/Oct-17-19.jpeg";
-import upevent12 from "../assets/img/August-30-31.jpeg";
-import upevent13 from "../assets/img/August-29-30-31.jpeg";
-import upevent14 from "../assets/img/August-8-9-10.jpeg";
+// import upevent1 from "../assets/img/nepal-pharma-dec23_page-0001.jpg";
+// import upevent2 from "../assets/img/Looking up Media partner.jpg";
+// import upevent3 from "../assets/img/pharma_live_expo-min.jpg";
+// import upevent4 from "../assets/img/amp_tech_haridwar-min.jpg";
+// import upevent5 from "../assets/img/ugandapharma2024-VB_page-0001.jpg";
+// import upevent6 from "../assets/img/26_April_Event.jpeg";
+// import upevent7 from "../assets/img/25-26_April_Event.jpeg";
+// import upevent8 from "../assets/img/12-14_April_Event.jpg";
+// import upevent9 from "../assets/img/April2025-17-18-19.jpeg";
+// import upevent10 from "../assets/img/Feb2025-20-21-22.jpeg";
+// import upevent11 from "../assets/img/Oct-17-19.jpeg";
+// import upevent12 from "../assets/img/August-30-31.jpeg";
+// import upevent13 from "../assets/img/August-29-30-31.jpeg";
+// import upevent14 from "../assets/img/August-8-9-10.jpeg";
+
+
+import upevent1 from "../assets/img/10_11_12_April2025_Event.jpg";
+import upevent2 from "../assets/img/10_11_12_April2025_Event2.jpg";
+import upevent3 from "../assets/img/17_18_19_April2025_Event.jpg";
+import upevent4 from "../assets/img/17_18_19_April2025_Event2.jpg";
+
+
+
 
 export default function UpcomingEvents() {
   const images = [
-    upevent14, upevent13, upevent12, upevent11, upevent10, upevent9,
-    upevent8, upevent7, upevent6, upevent5, upevent4, upevent3, upevent2, upevent1
+     upevent1, upevent2, upevent3, upevent4
   ];
 
   return (
